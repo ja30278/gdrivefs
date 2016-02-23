f6938b30d47d005a3f8327bd6dafa05fde6ee8b5
Jon Allie <jon@jonallie.com>
2016-02-22 19:29:13 -0500

    switched to device flow, dropped embedded HTTP server

--- src/bin/init_token.rs
+++ src/bin/init_token.rs
@@ -3,65 +3,29 @@ extern crate gdrivefs;
 extern crate hyper;
 extern crate inth_oauth2;
 extern crate rustc_serialize;
-extern crate urlparse;
 
 use std::default::Default;
-use std::str::FromStr;
-use urlparse::GetQuery;
+use std::io;
 use gdrivefs::oauth;
 
 // see: https://developers.google.com/identity/protocols/googlescopes
 const DRIVE_SCOPE : &'static str = "https://www.googleapis.com/auth/drive";
 
-fn fetch_oauth_token(client: &mut inth_oauth2::client::Client<inth_oauth2::provider::Google>,
-                     port: u16)
-                     -> inth_oauth2::token::Bearer<inth_oauth2::token::Expiring> {
-  println!("Authentication needed");
-  let (tx, rx) = std::sync::mpsc::channel();
-  let mut_tx = std::sync::Mutex::new(tx);
-  let addr = std::net::SocketAddrV4::new(std::net::Ipv4Addr::from_str("0.0.0.0").unwrap(), port);
-  let mut listener = hyper::Server::http(&addr)
-                       .unwrap()
-                       .handle(move |req: hyper::server::Request,
-                                     mut resp: hyper::server::Response| {
-                         match req.uri {
-                           hyper::uri::RequestUri::AbsolutePath(path) => {
-                             let parsed_url = urlparse::urlparse(&path);
-                             let code = parsed_url.get_parsed_query()
-                                                  .and_then(|q| q.get_first_from_str("code"));
-                             if code.is_none() {
-                               *resp.status_mut() = hyper::status::StatusCode::NotImplemented;
-                               return;
-                             }
-                             mut_tx.lock().unwrap().send(code.unwrap()).unwrap();
-                             resp.send(b"ok").unwrap();
-                           }
-                           _ => *resp.status_mut() = hyper::status::StatusCode::NotImplemented,
-                         };
-                       })
-                       .unwrap();
-  println!("Visit URL to authorize");
-  let auth_uri = client.auth_uri(Some(DRIVE_SCOPE), None).unwrap();
-  println!("{}", auth_uri);
-  let code = rx.recv().unwrap();
-  listener.close().unwrap();
-  let http_client = Default::default();
-  let token_result = client.request_token(&http_client, &code).unwrap();
-  println!("got token: {:?}", token_result);
-  token_result
-}
+// the auth URI used for the 'application' auth flow.
+// see: https://developers.google.com/identity/protocols/OAuth2InstalledApp#choosingredirecturi
+const OOB_AUTH_URI: &'static str = "urn:ietf:wg:oauth:2.0:oob";
+
 
 const USAGE: &'static str = "
 init_token: fetch and store an oauth2 token for gdrivefs.
 
 Usage:
-  init_token --client-id-file=<id_file> --client-secret-file=<secret_file> --token-file=<token_file>  [--port=<port>]
+  init_token [--client-id-file=<id_file>] [--client-secret-file=<secret_file>] [--token-file=<token_file>]  [--port=<port>]
 
 Options:
-  --client-id-file=<id_file>          File containing a client id [default: /etc/gdrive_id]
-  --client-secret-file=<secret_file>  File containing a client secret. [default: /etc/gdrive_secret]
+  --client-id-file=<id_file>          File containing a client id [default: /usr/local/etc/gdrive_id]
+  --client-secret-file=<secret_file>  File containing a client secret. [default: /usr/local/etc/gdrive_secret]
   --token-file=<token_file>           Token output file. [default: /etc/gdrive_token]
-  --port=<port>                       Port on which to listen for redirects. [default: 8080]
 ";
 
 #[derive(Debug, RustcEncodable, RustcDecodable)]
@@ -69,21 +33,27 @@ struct Args {
   flag_client_id_file: String,
   flag_client_secret_file: String,
   flag_token_file: String,
-  flag_port: u16,
 }
 
 fn main() {
   let args: Args = docopt::Docopt::new(USAGE).and_then(|d| d.decode()).unwrap_or_else(|e| e.exit());
-  println!("Client id = {}, secret = {}, output file = {}, port = {}",
-           &args.flag_client_id_file,
-           &args.flag_client_secret_file,
-           &args.flag_token_file,
-           &args.flag_port);
-  let mut client = oauth::new_google_client(
-    &gdrivefs::get_contents(&args.flag_client_id_file).unwrap(),
-    &gdrivefs::get_contents(&args.flag_client_secret_file).unwrap(),
-    Some(format!("http://localhost:{}/oauth_redirect", args.flag_port)));
-  let token = fetch_oauth_token(&mut client, args.flag_port);
+  let client = oauth::new_google_client(
+      &gdrivefs::get_contents(&args.flag_client_id_file).unwrap(),
+      &gdrivefs::get_contents(&args.flag_client_secret_file).unwrap(),
+      Some(OOB_AUTH_URI.into()));
+
+  println!("Please visit the following URL to grant the required permissions");
+  println!("Then paste the returned code below.");
+  let auth_uri = client.auth_uri(Some(DRIVE_SCOPE), None).unwrap();
+  println!("{}", auth_uri);
+  println!("Code: ");
+
+  let mut code: String = String::new();
+  io::stdin().read_line(&mut code).unwrap();
+  println!("got code: {}, requesting token", code);
+
+  let http_client = Default::default();
+  let token = client.request_token(&http_client, &code).unwrap();
   oauth::save_token(&args.flag_token_file, &token).unwrap();
   println!("Saved token in {}", args.flag_token_file);
 }
